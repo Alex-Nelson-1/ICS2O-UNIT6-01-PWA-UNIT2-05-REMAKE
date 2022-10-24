d63fa2d73db4ce717ae6b4d29413231af5b4280d
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-24 18:44:30 +0000

    Changed to 2-05

--- js/script.js
+++ js/script.js
@@ -18,9 +18,19 @@ if (navigator.serviceWorker) {
   )
 }
 
-/**
- * This function displays an alert.
- */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+/* This function calculates a users earnings and displays it */
+function calculatePay() {
+  /* Input */
+  const hoursWorked = parseFloat(document.getElementById("hoursWorked").value)
+  const wage = parseFloat(document.getElementById("hourlyRate").value)
+  const TAX_RATE = 0.18
+  /* Proccess */
+  const pay = hoursWorked * wage * (1 - TAX_RATE)
+  const taxes = hoursWorked * wage * TAX_RATE
+  /* Output */
+  document.getElementById("pay").innerHTML = `Your pay will be: $ ${pay.toFixed(
+    2
+  )}`
+  document.getElementById("governmentPortion").innerHTML =
+    "The government will take: $" + taxes.toFixed(2)
 }
